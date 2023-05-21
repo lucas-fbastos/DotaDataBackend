8696a869a472bf7ee73a2e8099c7eb2e4642907e
lucas-fbastos <felixbastos.lucas@gmail.com>
2023-05-21 02:15:02 +0100

    fix: handling for entity not found exception

--- src/main/java/com/br/app/DotaTrainerBackend/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/br/app/DotaTrainerBackend/resource/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.br.app.DotaTrainerBackend.resource.exceptions;
 
 
 import com.br.app.DotaTrainerBackend.exception.ExternalApiException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ExternalApiException.class)
-    public ResponseEntity<StandardError> StandardError(ExternalApiException e, HttpServletRequest req){
+    public ResponseEntity<StandardError> externalApiError(ExternalApiException e, HttpServletRequest req){
         StandardError error = new StandardError(HttpStatus.INTERNAL_SERVER_ERROR.value(),e.getMessage(),System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ExternalApiException e){
+        StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(),e.getMessage(),System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
