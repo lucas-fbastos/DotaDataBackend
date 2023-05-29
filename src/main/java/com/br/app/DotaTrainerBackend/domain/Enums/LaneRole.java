a8924aabc01eb4f97df5aa43b60d088724d72543
lucas-fbastos <felixbastos.lucas@gmail.com>
2023-05-29 23:31:16 +0100

    feat: add lane role enum

---
+++ src/main/java/com/br/app/DotaTrainerBackend/domain/Enums/LaneRole.java
@@ -0,0 +1,20 @@
+package com.br.app.DotaTrainerBackend.domain.Enums;
+
+public enum LaneRole {
+
+    CARRY(1),
+    NUKER(2),
+    INITIATOR(3),
+    DISABLER(4),
+    DURABLE(5),
+    ESCAPE(6),
+    SUPPORT(7),
+    PUSHER(8),
+    JUNGLER(9);
+
+    final int id;
+
+    LaneRole(int id) {
+        this.id = id;
+    }
+}
